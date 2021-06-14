6cf9b6d136d38a3ef216f2f36fe01825104ae6e5
Pavel Mineev <pavel@mineev.me>
2021-06-14 12:21:46 +0300

    xbox catalog

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+	mode: 'jit',
+	purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			sans: '"Segoe UI", SegoeUI, "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
+		},
+		extend: {
+			colors: {
+				green: colors.green,
+				gray: colors.trueGray,
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
